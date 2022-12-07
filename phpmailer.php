17883098fb260c4109c79ca7452d9ffc5d520125
GNUNIXLOOTER <andresparradoparrado@gmail.com>
2022-12-06 19:24:50 -0500

    nuevo archivo php para enviar mensajes

---
+++ phpmailer.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['email'])) {
+    
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "andresparradoparrado@gmail.com";
+$email_subject = "Contacto desde mi sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['name']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['email']) ||
+!isset($_POST['message'])) {
+
+echo "<b>Ocurrió un error y el mensaje no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "Apellido: " . $_POST['subject'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Teléfono: " . $_POST['message'] . "\n";
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El mensaje se ha enviado con éxito!";
+}
+?>
