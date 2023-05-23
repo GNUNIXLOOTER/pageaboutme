a034a4d175f662af5962e334c254c06bd7fbaab5
PABLOANDRESPARRADO <67743030+GNUNIXLOOTER@users.noreply.github.com>
2023-05-23 14:12:20 -0500

    se deshacen los cambio

---
+++ phpmailer.php
@@ -0,0 +1,37 @@
+<?php
+   //Reseteamos variables a 0.
+   $name = $subject = $email = $message = $para = $headers = $msjCorreo = NULL;
+
+   if (isset($_POST['submit'])) {
+      //Obtenemos valores input formulario
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $subject = $_POST['subject'];   
+      $message = $_POST['message'];
+      $para = 'andresparradoparrado@gmail.com';
+
+      //Creamos cabecera.
+      $headers = 'From' . " " . $email . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $name;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Asunto: " . $subject;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Correo: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $message;
+      $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+         echo "<script language='javascript'>
+            alert('Mensaje enviado, te respondere a la brebedad.');
+         </script>";
+    } else {
+         echo "<script language='javascript'>
+            alert('No se pudo enviar el mensaje');
+         </script>";
+    }
+  }
+?>
